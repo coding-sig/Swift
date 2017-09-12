97d7108450011ca92bec8674559d8345f9057ca7
Wei-jye Wang <weijye91@live.com>
2017-09-13 00:14:36 +0800

    feat(board): add dragula and drag and drop to board settings

--- src/app/components/board/board-settings/board-settings.component.ts
+++ src/app/components/board/board-settings/board-settings.component.ts
@@ -2,15 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { TabViewModule } from 'primeng/primeng';
 import { InputTextModule } from 'primeng/primeng';
 import { DragDropModule } from 'primeng/primeng';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
 	selector: 'app-board-settings',
 	templateUrl: './board-settings.component.html',
-	styleUrls: ['./board-settings.component.scss']
+	styleUrls: ['./board-settings.component.scss'],
+	animations: [
+		trigger('listAnimation', [
+			transition('* => *', [ // each time the binding value changes
+				query(':leave', [
+					stagger(100, [
+						animate('0.5s', style({ opacity: 0 }))
+					])
+				]),
+				query(':enter', [
+					style({ opacity: 0 }),
+					stagger(100, [
+						animate('0.5s', style({ opacity: 1 }))
+					])
+				])
+			])
+		])
+	]
 })
 export class BoardSettingsComponent implements OnInit {
 
-	constructor() { }
+	constructor(private dragulaService: DragulaService) {
+		dragulaService.setOptions('columns-container-bag', {
+			moves: function (el, container, handle) {
+				return handle.className === 'mdi mdi-drag';
+			},
+			direction: 'horizontal'
+		});
+
+		dragulaService.setOptions('column-statuses-bag', {
+			// moves: function (el, container, handle) {
+			// 	return handle.className.includes('status-card');
+			// },
+			// direction: 'horizontal'
+		});
+	 }
 	mockColumnConfig = [
 		{ title: 'To Do', statuses: [ 'TO DO' ] },
 		{ title: 'In Progress', statuses: [ 'IN PROGRESS' ] },
@@ -19,11 +52,7 @@ export class BoardSettingsComponent implements OnInit {
 	];
 
 	unmappedStatuses = [ 'QA TEST', 'LINTING' ];
-
-	drop(event) {
-		console.log(event);
-		window.alert('Drop event' + event);
-	}
+	
 
 	ngOnInit() {
 	}
--- src/app/components/board/board.module.ts
+++ src/app/components/board/board.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabViewModule } from 'primeng/primeng';
 import { InputTextModule } from 'primeng/primeng';
-import { DragDropModule } from 'primeng/primeng';
+import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardComponent } from './board.component'; 
@@ -15,7 +15,7 @@ import { StoryDetailsComponent } from '../story-details/story-details.component'
         BoardRoutingModule,
         TabViewModule,
         InputTextModule,
-        DragDropModule
+        DragulaModule
     ],
     declarations: [ 
         BoardComponent,
